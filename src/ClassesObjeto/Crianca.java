c628c90ef0a7fd1ca20acbb563bf13fff21bf039
04035054 <brunoeloi12@gmail.com>
2019-03-13 20:47:28 -0300

    Initial commit

---
+++ src/ClassesObjeto/Crianca.java
@@ -0,0 +1,24 @@
+package ClassesObjeto;
+
+class Crianca {
+
+    String corcabelo;
+    String corpele;
+    double altura;
+    int idade;
+
+    void brinca(){
+        System.out.println("Brincando...");
+    }
+    void correr(double metros){
+        System.out.println("Correndo " + metros + " metros...");
+    }
+    void nadar (double metros){
+        System.out.println("Nadadando " + metros + " m");
+    }
+    String Sorrir(){
+        return ":)";
+    }
+}
+
+
